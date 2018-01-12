a8e42790da2d7697207fb370ca1031da252662b5
super-amazing-joey-boy <joseph.slomowitz@gmail.com>
2018-01-12 17:00:54 +1100

    Testing New Map Feature in this branch
    -New File `MapForItinerary.swift`

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Joey Slomowitz on 12/1/18.
+//  Copyright © 2018 Joey Slomowitz. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
